3baaa5f53b723f71c1ea733c629e4afdbe9642d6
forosuru <45704989+forosuru@users.noreply.github.com>
2019-02-20 14:33:37 +0000

    users.yml, create/rename pages for users and languages if necessary

--- update.py
+++ update.py
@@ -9,6 +9,7 @@ import socket
 import os
 import re
 import sys
+import yaml
 
 prefix_dir = '_events/'
 event_file_extension = '.html'
@@ -34,18 +35,108 @@ def events_from_xml_string(data):
         eauthor = e.find('author').find('name').text
         edate = e.find('published').text            
         event = {
-            'link':elink,
-            'title':etitle,
-            'id':eid,
-            'content':econtent, 
-            'author_name':eauthor,
-            'date':edate,
-            'author_id':author_id,
-            'type':event_type,
-            'language':language
+            'link'          :elink,
+            'title'         :etitle,
+            'id'            :eid,
+            'content'       :econtent, 
+            'author_name'   :eauthor,
+            'date'          :edate,
+            'author_id'     :author_id,
+            'type'          :event_type,
+            'language'      :language
             }
         res.append(event)
     return res
+
+# logic: list all events by id ascending
+# for each event
+#   if language does not exist in _lang/ then create it
+# for each author_id:
+#   if login from yml file does not match event author name:
+#       delete _user/login.html file
+#       update yml dictionary, set flag
+#   if _user/login.html does not exist, create it
+# if flag, dump users dictionary into yml file
+
+def language_filename(lang):
+    tr = {'+':'plus', '#':'sharp', '*':'star', '\'': ''}  # C++ => Cplusplus, C# => Csharp, F* => Fstar
+    lang_tr = ''
+    for c in list(lang):
+        if c.isalnum():
+            lang_tr += c
+        else:
+            if c in tr:
+                lang_tr += tr[c]
+            else:
+                lang_tr += '_'
+    return '_lang/' + lang_tr + '.html'
+
+
+def lang_fwrite(lfn, lang):
+    data = [
+            '---',
+            'layout: default',
+            'language: ' + lang,
+            'filename: ' + os.path.basename(lfn),
+            '---',
+            '{% assign events = site.events | where: "language","' + lang + '" | sort: "date" | reverse %}',
+            '{% for event in events %}',
+            '{{ event.content }}',
+            '{% endfor %}'
+        ]
+    with open(lfn, 'w') as fp:
+        fp.write('\n'.join(data))
+        fp.close()
+
+def user_fwrite(fn, uid, u):
+    data = [
+            '---',
+            'layout: default',
+            'intra: ' + u['intra'],
+            'login: ' + u['login'],
+            'id: ' + str(uid),
+            'avatar_url: ' + u['avatar_url'],
+            '---',
+            '{% assign events = site.events | where: "author_id","' + str(uid) + '" | sort: "date" | reverse %}',
+            '{% for event in events %}',
+            '{{ event.content }}',
+            '{% endfor %}'
+        ]
+
+    with open(fn, 'w') as fp:
+        fp.write('\n'.join(data))
+        fp.close()
+
+def update_pages(events):
+    update_usersf = False
+    all_users = None
+    with open('_data/users.yml', 'r') as fp:
+        all_users = yaml.load(fp)
+        fp.close()
+
+    for e in events:
+        lfn = language_filename(e['language'])
+        if e['language'] != 'None':
+            if not os.path.exists(lfn):
+                lang_fwrite(lfn, e['language'])
+
+        uid = int(e['author_id'])
+        if all_users[uid]['login'] != e['author_name']:
+            ufn = '_user/' + all_users[uid]['login'] + '.html'
+            if os.path.exists(ufn): #if they changed their github name
+                os.unlink(ufn)      #remove existing file
+            update_usersf = True
+            all_users[uid]['login'] = e['author_name']
+
+        ufn = '_user/' + all_users[uid]['login'] + '.html'
+        if not os.path.exists(ufn):
+            user_fwrite(ufn, uid, all_users[uid])
+
+    if update_usersf:
+        with open('_data/users.yml', 'w') as fp:
+            yaml.dump(all_users, fp, default_flow_style=False)
+            fp.close()
+
 
 def fetch_events(page_num):   #page_num numeric page number
     url = github_atom_url + '&page=' + str(int(page_num))
@@ -86,15 +177,15 @@ def event_fwrite(e, fn):
         e['content']
     ]
     event_mkdir(e['date'])
-    with open(fn, 'w+') as fp:
+    with open(fn, 'w') as fp:
         fp.write('\n'.join(fdata))
         fp.close()
-
 
 def forosuru_update():
     i = 1
     done = False
     authors = set()
+    new_events = []
     while not done:
         done = True
         events = fetch_events(i)
@@ -104,7 +195,9 @@ def forosuru_update():
                 done = False
                 event_fwrite(e, fn)
                 authors.add(e['author_name'])
+                new_events.append(e)
         i += 1
+    update_pages(sorted(new_events, key=lambda k: k['id']))
     return authors
 
 def is_online(host="8.8.8.8", port=53, timeout=3):      # returns true if we can ping google
